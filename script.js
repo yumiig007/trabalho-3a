60a0c5ab6f225b88de567d0918acd41718cd181d
yumiig007 <00001122193075sp@al.educacao.sp.gov.br>
2025-09-05 16:14:22 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+ 
+    botaoDeAcessibilidade.addEventListener('click', function (){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+ 
+     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+   
+    })
+ 
+     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+     
+     const alternaContraste = document.getElementById('alterna-contraste')
+ 
+     let tamanhoAtualFonte = 1;
+ 
+     aumentaFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+     })
+ 
+     diminuiFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte -= 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+ 
+     })
+ 
+     alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+         console.log("Contraste alternado:", document.body.classList.contains("alto-contraste"));
+});
+     })
